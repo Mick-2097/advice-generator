81e65a1922148005860a5f2e13967da1dd2ac790
Mick <mix2444@gmail.com>
2022-03-28 17:44:31 +1100

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.dice')
+function getAdvice() {
+
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('h1').innerText = 'ADVICE #' + data.slip.id
+        document.querySelector('p').innerText = `"${data.slip.advice}"`
+    })
+}
+button.addEventListener('click', event => {
+    getAdvice()
+})
+
